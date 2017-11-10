2093c0feac230e6d7a5cd0a3cec398393fc15b0f
nikk4s <33534765+nikk4s@users.noreply.github.com>
2017-11-10 11:23:51 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -15,6 +15,44 @@
 #define RS 4
 #define E  5
 
+// constants
+
+/************************************************************************/
+/* LCD Delay                                                                     */
+/************************************************************************/
+uint8_t kDelay = 50;
+
+/************************************************************************/
+/* Limit base 60                                                                     */
+/************************************************************************/
+uint8_t kBase60 = 59;
+
+/************************************************************************/
+/* Limit base 12                                                                    */
+/************************************************************************/
+uint8_t kBase12 = 12;
+
+/************************************************************************/
+/* ASCII offset value to digit                                                                    */
+/************************************************************************/
+uint8_t kOffset = 48;
+
+/************************************************************************/
+/* ASCII colon                                                                    */
+/************************************************************************/
+uint8_t kColon = 58;
+
+/************************************************************************/
+/* Base 10 divisor                                                                    */
+/************************************************************************/
+uint8_t kDivisor = 10;
+
+/************************************************************************/
+/* Characters for time display                                                                     */
+/************************************************************************/
+uint8_t kDisplayCharacters = 8;
+
+
 // declare functions
 void send_command (unsigned char command);
 void send_data (unsigned char character);
@@ -29,15 +67,15 @@ int main(void)
 	send_command(0x0E);// screen and cursor ON
 	
 	// declare time counters and set start time
-	uint8_t hrs = 7;
-	uint8_t mins = 45;
+	uint8_t hrs = 11;
+	uint8_t mins = 20;
 	uint8_t secs = 0;
 	// initialize display arrays
 	char hours[2] = {0,0};
 	char minutes[2] = {0,0};
 	char seconds[2] = {0,0};
 	// initialize display string
-	char time[8] = {hours[0], hours[1], 58, minutes[0], minutes[1], 58, seconds[0], seconds[1]};
+	char time[8] = {hours[0], hours[1], kColon, minutes[0], minutes[1], kColon, seconds[0], seconds[1]};
 	// send first line to the LCD display
 	char message1[] = "Current Time";
 	uint8_t length1 = strlen(message1);
@@ -56,7 +94,7 @@ int main(void)
 	
 		secs++;
 		
-		if (secs > 59)
+		if (secs > kBase60)
 		{
 			secs = 0;
 			mins++;
@@ -65,7 +103,7 @@ int main(void)
 		{
 			//DoNothing();
 		}
-		if (mins > 59)
+		if (mins > kBase60)
 		{
 			mins = 0;
 			hrs++;
@@ -75,7 +113,7 @@ int main(void)
 			//DoNothing();
 		}
 
-		if (hrs > 12)
+		if (hrs > kBase12)
 		{
 			hrs = 1;
 		}
@@ -85,49 +123,49 @@ int main(void)
 		}
 
 		
-		if ((secs / 10)>0)
+		if ((secs / kDivisor)>0)
 		{
-			seconds[0] = ((secs/10)+48);
-			seconds[1] = ((secs%10)+48);
+			seconds[0] = ((secs/kDivisor)+kOffset);
+			seconds[1] = ((secs%kDivisor)+kOffset);
 		}
 		else
 		{
-			seconds[0] = (0+48);
-			seconds[1] = (secs+48);
+			seconds[0] = (0+kOffset);
+			seconds[1] = (secs+kOffset);
 		}
 		
-		if ((mins / 10)>0)
+		if ((mins / kDivisor)>0)
 		{
-			minutes[0] = ((mins/10)+48);
-			minutes[1] = ((mins%10)+48);
+			minutes[0] = ((mins/kDivisor)+kOffset);
+			minutes[1] = ((mins%kDivisor)+kOffset);
 		}
 		else
 		{
-			minutes[0] = (0+48);
-			minutes[1] = (mins+48);
+			minutes[0] = (0+kOffset);
+			minutes[1] = (mins+kOffset);
 		}
 		
-		if ((hrs / 10)>0)
+		if ((hrs / kDivisor)>0)
 		{
-			hours[0] = ((hrs/10)+48);
-			hours[1] = ((hrs%10)+48);
+			hours[0] = ((hrs/kDivisor)+kOffset);
+			hours[1] = ((hrs%kDivisor)+kOffset);
 		}
 		else
 		{
-			hours[0] = (0+48);
-			hours[1] = (hrs+48);
+			hours[0] = (0+kOffset);
+			hours[1] = (hrs+kOffset);
 		}
 		
 		time[0] = hours[0];
 		time[1] = hours[1];
-		time[2] = 58;
+		time[2] = kColon;
 		time[3] = minutes[0];
 		time[4] = minutes[1];
-		time[5] = 58;
+		time[5] = kColon;
 		time[6] = seconds[0];
 		time[7] = seconds[1];
 			
-		uint8_t length2 = 8;
+		uint8_t length2 = kDisplayCharacters;
 		
 		send_command(0xC0); // Move to beginning of second line  //adds 50ms delay
 
